73e87da43e35678793394053bf626c784e592ec9
noboby cares <student71018@carrollcountyschools.com>
2022-12-08 18:13:36 +0000

    Add new file

---
+++ static/src/js/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/prox/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: '/src/js/uv/uv.handler.js',
+    bundle: '/src/js/uv/uv.bundle.js',
+    config: '/src/js/uv/uv.config.js',
+    sw: '/src/js/uv/uv.sw.js',
+};
