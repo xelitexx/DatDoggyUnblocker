b64830b8cec0c693624a0dd6269aabe4ab8382a2
noboby cares <student71018@carrollcountyschools.com>
2024-01-23 13:32:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-
 (async() => {
     await import('./index.mjs');
   })();
+  const routes = [
+  { path: '/', file: 'who-let-him-cook.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+  { path: '/blog', file: 'widget.html' },
+];
   
   
 
