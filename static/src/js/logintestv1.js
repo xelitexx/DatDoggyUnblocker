d9c12eec614c69fda8aeb4c3fb5310bf53ef614f
noboby cares <student71018@carrollcountyschools.com>
2024-04-25 19:45:54 +0000

    Update logintestv1.js

--- static/src/js/logintestv1.js
+++ static/src/js/logintestv1.js
@@ -3,18 +3,19 @@ function login() {
     var accessUser = "Zombi3"; 
     var enteredUser = document.getElementById("user").value; 
     var enteredPass = document.getElementById("pass").value;
-
-encryptCode(code) { 
+}
+encryptCode(code); { 
 var salt = "kJdFhGK";
 var iv = "789012345678901234567890123456"; 
 var key = "GHJKLmNpQRSTUVwX"; 
 var encryptedCode = CryptoJS.AES.encrypt(code, key, { iv: iv, salt: salt }).toString();
-return encryptedCode; }
+return encryptedCode; 
+}
 
 
 var encryptedPass = encryptCode(accessPass);
 
-if ((encryptedCode === enteredPass) && (enteredUser === accessUser) { 
+if ((encryptedCode === enteredPass) && (enteredUser === accessUser)) { 
 window.location.href = "dev.html"; } 
 else {
-document.getElementById("error").textContent = "Invalid access code!"; } }
+document.getElementById("error").textContent = "Invalid access code!"; }
