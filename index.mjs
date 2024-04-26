3f0c2bbb9372b521e5d2c09cdccd9dcaa23bd1a1
noboby cares <student71018@carrollcountyschools.com>
2024-04-26 16:51:11 +0000

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,19 +2,6 @@ import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
-const routes = [
-  { path: '/', file: 'who-let-him-cook.html' },
-  { path: '/news', file: 'apps.html' },
-  { path: '/events', file: 'games.html' },
-  { path: '/diagnostic', file: 'settings.html' },
-  { path: '/local-news', file: 'tabs.html' },
-  { path: '/image-galleries', file: 'go.html' },
-  { path: '/blog', file: 'widget.html' },
-  { path: '/home', file: 'home.html' },
-  { path: '/contact', file: 'contact.html' },
-  { path: '/about', file: 'about.html' },
-];
-
 const bare = createServer('/bare/');
 const serve = new nodeStatic.Server('static/');
 
